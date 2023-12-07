951225b336e16afbbaa51c7ece89f418d5b5a86a
root <root@ip-172-31-44-234.ec2.internal>
2023-12-07 08:34:24 +0000

    cicd

---
+++ .github/workflows/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SSH_USER="root"
+SERVER_IP="34.235.168.103"
+DEPLOY_PATH="/var/www/cicd"
+
+echo "Deploying to server..."
+scp -r ./build/* $SSH_USER@$SERVER_IP:$DEPLOY_PATH
+echo "Deployment completed."
